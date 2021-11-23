0a3f4c6e46702ddacdbf37f86831d3ae1dfd43a5
Samker <samkergachu@gmail.com>
2021-11-23 08:23:19 +0300

    Add a new Game.kt file

---
+++ src/Game.kt
@@ -0,0 +1,45 @@
+fun main(args: Array<String>) {
+    val name = "Madrigal"
+    val healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
+
+    // Aura
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+    // Player status
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds, but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
+    return healthStatus
+}
+
+private fun printPlayerStatus(auraColor: String, isBlessed: Boolean, name: String, healthStatus: String) {
+    println("(Aura: $auraColor) " +
+            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    return if (auraVisible) "GREEN" else "NONE"
+}
+
+private fun castFireBall() {
+    println("A glass of Fireball springs to existence.")
+}
---
+++ src/TypeIntro.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    //state is either one or zero
+    //var hasSteed = 1 //1 represents absent
+    //val pubName = "Unicorn's horn"
+    var playerName = "Estragon"
+    //var currentPublican = "Nobody"
+    //var amountOfGold = 50;
+    //var menu = listOf("Chai", "Mandazi", "Kuku")
+
+    //reversed string
+    val reversed = playerName.reversed()
+
+    println(reversed)
+
+
+
+
+
+
+}
